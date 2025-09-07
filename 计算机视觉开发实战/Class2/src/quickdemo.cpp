c24924d569b3b32c56e2f6636a62eb21d6eb2d95
zhengboyang <zbyyyds@qq.com>
2025-09-07 22:20:01 +0800

    优化了第六节课的内容：滚动条操作演示-参数传递度

--- 计算机视觉开发实战/Class2/src/quickdemo.cpp
+++ 计算机视觉开发实战/Class2/src/quickdemo.cpp
@@ -95,21 +95,31 @@ void QuickDemo::operators_demo(Mat &image)
     imshow("add",dst);
 }
 
-Mat src,dst,m;
-int lightness = 50;
+static void on_lightness(int b,void* userdata){
+    Mat image = *((Mat*)userdata);
+    Mat dst = Mat::zeros(image.size(),image.type());
+    Mat m = Mat::zeros(image.size(),image.type());
+    m = Scalar(b,b,b);
+    addWeighted(image,1.0,m,0,b,dst);
+    imshow("亮度调整",dst);
+}
 
-static void on_track(int pos,void* userdata){
-    m = Scalar(lightness,lightness,lightness);
-    add(src,m,dst);
+static void on_contrast(int b,void* userdata){
+    Mat image = *((Mat*)userdata);
+    Mat dst = Mat::zeros(image.size(),image.type());
+    Mat m = Mat::zeros(image.size(),image.type());
+    double contrast = b/100.0;
+    addWeighted(image,contrast,m,0.0,0,dst);
     imshow("亮度调整",dst);
 }
 
 void QuickDemo::tracking_bar_demo(Mat &image){
-    namedWindow("亮度调整",WINDOW_AUTOSIZE);
-    dst = Mat::zeros(image.size(), image.type());
-    m = Mat::zeros(image.size(), image.type());
-    src = image;
+    namedWindow("亮度与对比度调整",WINDOW_AUTOSIZE);
+    int lightness = 50;
     int max_value = 100;
-    createTrackbar("Value Bar:","亮度调整",&lightness,max_value,on_track);
-    on_track(50,0);
+    int contrast_value = 100;
+
+    createTrackbar("Value Bar:","亮度与对比度调整",&lightness,max_value,on_lightness,(void*)(&image));
+    createTrackbar("Contrast Bar:","亮度与对比度调整",&contrast_value,200,on_contrast,(void*)(&image));
+    on_lightness(50,&image);
 }
